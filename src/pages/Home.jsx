a3e39ecbe1c222e4ddde9b2cb2ffc5b79254196c
manjarrezcristian <cristiantovarm@gmail.com>
2026-02-26 22:17:55 -0500

    se crea pagina home y se actualiza app

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <main>
+        <nav>
+            <Navbar />
+        </nav>
+        <section></section>
+        <footer>
+            <Footer />
+        </footer>
+    </main>
+  )
+}
+
+export default Home
